e8c8efb4117e0949c9a01738f15f1c584ef165ae
Stepaniuk-Den <128968908+Stepaniuk-Den@users.noreply.github.com>
2023-06-06 18:43:03 +0300

    update-01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const cardsList = document.querySelector(".gallery");
+
+const galleryList = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`
+  )
+  .join("");
+cardsList.insertAdjacentHTML("afterbegin", galleryList);
+cardsList.addEventListener("click", handleImgClick);
+
+function handleImgClick(evt) {
+  evt.preventDefault();
+  if (evt.target === evt.currentTarget) {
+    return;
+  }
+  const currentList = evt.target.closest(".gallery__image");
+  const sourceList = currentList.dataset.source;
+  const currentDescr = currentList.alt;
+
+  const instance = basicLightbox.create(`
+    <img
+    src="${sourceList}"
+    alt="${currentDescr}"
+  />
+`);
+  instance.show();
+  document.addEventListener("keydown", (evt) =>
+    evt.key === "Escape" ? instance.close() : null
+  );
+}
